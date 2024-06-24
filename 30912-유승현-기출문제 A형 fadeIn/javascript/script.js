f078b2628ff7a26003d0b47b461b8ef1414ff87d
vkdskqlem1 <121661896+vkdskqlem1@users.noreply.github.com>
2024-06-24 15:41:32 +0900

    Add files via upload

---
+++ 30912-유승현-기출문제 A형 fadeIn/javascript/script.js
@@ -0,0 +1,27 @@
+$('.main>li').mouseover(function () {
+    $(this).children('.sub').stop().slideDown();
+});
+
+$('.main>li').mouseout(function () {
+    $(this).children('.sub').stop().slideUp();
+});
+
+$('.slidelist li:gt(0)').hide();
+setInterval(function () {
+    $('.slidelist li:first-child').fadeOut()
+        .next().fadeIn()
+        .end().appendTo('.slidelist ul')
+    return false;
+}, 3000);
+
+$('.tabmenu>li>a').click(function(){
+    $(this).parent().addClass('active').siblings().removeClass('active'); return false;
+});
+
+$('.notice li:first').click(function(){
+    $('#modal').show();
+});
+
+$('.m3').click(function(){
+    $('#modal').hide();
+});
